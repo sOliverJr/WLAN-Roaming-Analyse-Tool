59af42242eb711cc21e652b4ccd444c95e0ac6c6
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2022-07-29 15:41:47 +0200

    Updated Instructions

--- application/main.py
+++ application/main.py
@@ -1,5 +1,8 @@
 # CURRENTLY, ONLY WORKING ON MAC
 # Needs to be run as superuser to get BSSID
+
+# Run 'sudo python3 -m main' in venv-terminal
+
 from utils.PingUtil import PingUtil
 from utils.RoaminUtil import RoamingUtil
 import time
@@ -15,5 +18,3 @@ if __name__ == '__main__':
     roaming_threat.join()
 
     ping_util.set_pinging(False)
-
-
