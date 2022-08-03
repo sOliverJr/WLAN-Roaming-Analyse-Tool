c58833e4fb1785fc45b12b9aaf4b883af1279331
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2022-08-03 14:43:06 +0200

    small updates

--- application/utils/FileWriter.py
+++ application/utils/FileWriter.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class FileWriter:
     responses = []
+    date = datetime.today().strftime('%Y.%m.%d-%H:%M:%S')
 
     def add_response(self, element):
         self.responses.append(element)
@@ -14,6 +15,6 @@ class FileWriter:
 
     def write_responses(self):
         to_write = np.asarray(self.responses)
-        pd.DataFrame(to_write).to_csv("logs/pings.csv", index_label="Index", header=['Date: ' + datetime.today().strftime('%Y-%m-%d %H:%M:%S')])
+        pd.DataFrame(to_write).to_csv("logs/pings_" + self.date + ".csv", index_label="Index", header=['Date: ' + self.date])
         self.responses = []
         print('Wrote Array to file.')
